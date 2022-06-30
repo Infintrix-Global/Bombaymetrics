87e1d8cca5c74fbe9a2501ce74d2db1ea6d9dd0b
Nidhi Mehta <nidhi.mehta@infintrixglobal.com>
2022-06-30 19:54:43 +0530

    Summary report of Investor to show Final consolidated Profit Loss for All investors

---
+++ Bombaymetrics/IPO/Reports/InvestorSummaryReport.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InvestorSummaryReport.aspx.cs" MasterPageFile="~/Main.Master" Inherits="Bombaymetrics.InvestorSummaryReport" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="head">
+</asp:Content>
+<asp:Content runat="server" ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1">
+    <div class="containerfluid py-3">
+        <h2 class="text-center text-uppercase">Report</h2>
+        <div class="card" style="margin: 0em 2em  0em 2em">
+            <div class="card-header text-uppercase text-white">
+                <h5>Investor Holding Report</h5>
+            </div>
+
+            <div class="card-body table-responsive">                
+
+                <asp:GridView ID="GridView1" ClientIDMode="Static" HeaderStyle-CssClass="tableheader text-white" ShowHeaderWhenEmpty="true" runat="server" AutoGenerateColumns="false" CssClass="table  table-hover">
+                    <EmptyDataTemplate>
+                        <div class="text-center">No record found</div>
+                    </EmptyDataTemplate>
+                    <Columns>
+                        <asp:TemplateField HeaderText="Investor Name">
+                            <ItemTemplate>
+                                <asp:HyperLink runat="server" Text='<%#Eval("UserName")%>' Target="_blank" NavigateUrl='<%# Eval("ID","~/IPO/Reports/InvestorProfitLoss.aspx?InvestorID={0}") %>' />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="AsOnDate" HeaderText="As On Date" />
+                         <asp:BoundField DataField="TotalShares" HeaderText="Total Shares" />
+                         <asp:BoundField DataField="HoldingPercentage" HeaderText="Holding Percentage" />
+                         <asp:BoundField DataField="Investment" HeaderText="Investment Value" />
+                         <asp:BoundField DataField="ProfitLoss" HeaderText="Profit Loss" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
--- ProfitLoss SP.sql
+++ ProfitLoss SP.sql
@@ -1,20 +1,36 @@
-declare @UserID int
-set @UserID=10;
+--USE [Bombaymetrics]
+GO
+/****** Object:  StoredProcedure [dbo].[GetInvestorProfitLossReport]    Script Date: 6/29/2022 5:34:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- ALTER date: <ALTER Date,,>
+-- Description:	<Description,,>
+-- =============================================
+--exec GetInvestorProfitLossReport 172
+alter PROCEDURE [dbo].[GetInvestorProfitLossReport]
+@UserID int
+AS
+BEGIN
+select UserName, PanNo from [User] where ID=@UserID
 
-
-create table #Temp
+ALTER table #Temp1
 (
     AsOnDate DateTime, 
-    Holding decimal(10,2), 
+    Holding bigint, 
     vwap decimal(10,2),
-    PrevInvestment decimal(10,2),
-    PrevHolding decimal(10,2), 
-    Prevvwap decimal(10,2)
+    PrevInvestment decimal(18,2),
+    Qty bigint, 
+    Prevvwap decimal(10,2),
+	ProfitLoss decimal(18,2)
 )
 
 DECLARE @AsOnDate date;
- declare @Holding decimal(10,2);
-  declare @Price decimal(10,2);
+declare @Holding bigint;
+declare @vwap decimal(10,2);
 
 DECLARE db_cursor1 CURSOR FOR 
 select F.AsOnDate,F.Holding,( select dbo.GetWeekAvgVwap(F.AsOnDate)) as [Apox Price] 
@@ -22,44 +38,44 @@ from Folio F
 where UserID=@UserID 
 order by F.asondate 
 OPEN db_cursor1  
-FETCH NEXT FROM db_cursor1 INTO @AsOnDate, @Holding  , @Price
+FETCH NEXT FROM db_cursor1 INTO @AsOnDate, @Holding , @vwap
 
 WHILE @@FETCH_STATUS = 0  
 BEGIN 
 --Print @AsOnDate
-insert into #Temp (AsOnDate,Holding,vwap) select @AsOnDate,@Holding  , @Price
-    
-	FETCH NEXT FROM db_cursor1 INTO @AsOnDate, @Holding  , @Price 
+declare @PrevAsonDate  Date
+declare @PrevInvestment  decimal(18,2)
+declare @Prevvwap decimal(10,2)
+declare @PrevHolding bigint
+declare @Qty bigint
+declare @ProfitLoss decimal(18,2)
+
+if @PrevAsonDate is null
+begin
+	set @Prevvwap = 0
+	set @Qty = @Holding
+	set @PrevInvestment =@Holding * @vwap
+	set @ProfitLoss=0
+end
+else
+begin
+	select @Prevvwap =  dbo.GetWeekAvgVwap(@PrevAsonDate)
+	set @Qty =  @Holding -@PrevHolding 
+	set @PrevInvestment = @PrevInvestment + (@Qty * @vwap)	  
+	set @ProfitLoss=(@Holding *@vwap) -@PrevInvestment
+end
+
+set @PrevAsonDate= @AsOnDate	
+set @PrevHolding =@Holding
+
+insert into #Temp1 (AsOnDate,Holding,vwap,PrevInvestment,Qty, Prevvwap, ProfitLoss) select @AsOnDate,@Holding , @vwap, @PrevInvestment, @Qty,@Prevvwap, @ProfitLoss
+FETCH NEXT FROM db_cursor1 INTO @AsOnDate, @Holding  , @vwap 
 END 
 
 CLOSE db_cursor1
 DEALLOCATE db_cursor1
 
-DECLARE db_cursor4 CURSOR FOR 
-select AsOnDate,Holding ,vwap from #Temp
-OPEN db_cursor4  
-FETCH NEXT FROM db_cursor4 INTO @AsOnDate, @Holding  , @Price
+select convert(varchar,AsOnDate, 1) as [Investment Date],Qty,isnull(vwap,0) as [Apox VWP price],Holding, PrevInvestment as [Investment value], ProfitLoss from #Temp1
 
-WHILE @@FETCH_STATUS = 0  
-BEGIN 
-Print @AsOnDate
-update #Temp set PrevHolding=isnull((select Holding  from  dbo.GetPreviousAsonDateForUser(@UserID,@AsOnDate) ),0),
-Prevvwap=isnull((select vwap  from  dbo.GetPreviousAsonDateForUser(@UserID,@AsOnDate) ),0),
-PrevInvestment=isnull((select [Investment value]  from  dbo.GetPreviousAsonDateForUser(@UserID,@AsOnDate) ),vwap * Holding)
-where #Temp.AsOnDate= @AsOnDate and #Temp.Holding= @Holding  and #Temp.vwap= @Price
-    
-      FETCH NEXT FROM db_cursor4 INTO @AsOnDate, @Holding  , @Price 
-END 
-
-CLOSE db_cursor4
-DEALLOCATE db_cursor4
-select convert(varchar,AsOnDate, 1) as [Investment Date],Holding - PrevHolding as [Qty],PrevInvestment,
-isnull(vwap,0) as [Apox VWP price], Holding, 
-case when (Holding -PrevHolding =0)
-	Then (PrevInvestment)
-	else 
-	((Holding - PrevHolding) *vwap) + PrevInvestment
-	end
- as [Investment value], PrevHolding from #Temp
-
-drop table #Temp
+drop table #Temp1		
+END
